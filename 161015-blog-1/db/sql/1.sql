ada0b170602d6a8fe5e163e969b5484e67e0eb21
sainthkh <sainthkh@naver.com>
2016-10-17 10:53:12 +0900

    Moved Sql File.

--- 161015-blog-1/db/sql/1.sql
+++ 161015-blog-1/db/sql/1.sql
@@ -14,7 +14,7 @@ create table users(
 
 create table content_types(
 	id int not null primary key,
-	type varchar(255) not null,
+	type_name varchar(255) not null
 );
 
 create table groups(
@@ -25,30 +25,36 @@ create table groups(
 create table permissions(
 	id int not null primary key,
 	name varchar(255) not null,
-	foreign key (content_type_id) references content_type(id),
-	code_name varchar(100) not null
+	content_type_id int not null,
+	code_name varchar(100) not null,
+	foreign key (content_type_id) references content_types(id)
 );
 
 create table group_permissions(
 	id int not null primary key,
+	group_id int not null,
+	permission_id int not null,
 	foreign key (group_id) references groups(id),
 	foreign key (permission_id) references permissions(id)
 );
 
 create table user_permissions(
 	id int not null primary key,
+	user_id int not null,
+	permission_id int not null,
 	foreign key (user_id) references users(id),
 	foreign key (permission_id) references permissions(id)
-)
+);
 
 create table posts(
 	id int not null primary key,
 	title text not null,
 	slug varchar(250) not null,
-	foreign key (user_id) references users(id),
+	user_id int not null,
 	body text,
 	publish date not null,
 	created date not null,
 	updated date not null,
-	status varchar(10)
+	status varchar(10),
+	foreign key (user_id) references users(id)
 );
