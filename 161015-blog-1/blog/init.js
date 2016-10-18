ed1a93901b50d6a9f66dc480fad97d215252f33e
sainthkh <sainthkh@naver.com>
2016-10-18 10:36:08 +0900

    Applied model functions to main page.

--- 161015-blog-1/blog/init.js
+++ 161015-blog-1/blog/init.js
@@ -1,11 +1,25 @@
+const model = require('./model')
+const truncate = require('truncate')
+
 function init(app) {
 	app.get('/', main)
+	app.get('/:slug', details)
 }
 
 function main(req, res) {
-	res.render('blog/home', {
-		title: "My Blog"
+	model.get_posts_by_page(1, 5, function(posts){
+		res.render('blog/home', {
+			title: "My Blog",
+			posts: posts.map(function(post){
+				post.body = truncate(post.body, 200)
+				return post
+			})
+		})
 	})
+}
+
+function details(req, res) {
+	
 }
 
 module.exports = init
