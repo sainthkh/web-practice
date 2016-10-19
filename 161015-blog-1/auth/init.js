148e882270b273aba1e8985506f7554eaa6885b8
sainthkh <sainthkh@naver.com>
2016-10-19 10:30:14 +0900

    Added signup form.

--- 161015-blog-1/auth/init.js
+++ 161015-blog-1/auth/init.js
@@ -1,11 +1,15 @@
 function init(app) {
 	app.get('/login', login)
+	app.get('/signup', signup)
 }
 
 function login(req, res) {
 	res.render('auth/login')
 }
 
+function signup(req, res) {
+	res.render('auth/signup')
+}
 
 
 module.exports = init
