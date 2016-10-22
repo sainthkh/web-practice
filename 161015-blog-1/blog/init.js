e0fabb3e0591fc37e4c56e38a95859821c407471
sainthkh <sainthkh@naver.com>
2016-10-22 18:06:42 +0900

    Added time format.

--- 161015-blog-1/blog/init.js
+++ 161015-blog-1/blog/init.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const moment = require('moment')
+
 const model = require('./model')
 const auth = require('../auth')
 const user = auth.model
@@ -19,6 +21,7 @@ function main(req, res) {
 			title: "My Blog",
 			posts: posts.map(function(post){
 				post.body = truncate(post.body, 200)
+				post.publish = moment(post.publish).format("MMM Do, YYYY")	
 				return post
 			})
 		})
